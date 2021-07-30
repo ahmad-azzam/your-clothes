0e38e9d2f2b6d81bcdce1722648ce3fd54dc6054
ahmad-azzam <ahmadazzam123654@gmail.com>
2021-07-30 07:13:51 +0700

    first deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session');
 const router = require('./routes/index')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const path = require('path')
 
 app.set('view engine', 'ejs')
