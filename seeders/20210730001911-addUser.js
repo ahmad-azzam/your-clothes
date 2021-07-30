3f482904334eb8ebd13ebe4bb4ccd0fab44af2cc
ahmad-azzam <ahmadazzam123654@gmail.com>
2021-07-30 07:39:22 +0700

    first deploy

--- seeders/20210730001911-addUser.js
+++ seeders/20210730001911-addUser.js
@@ -6,7 +6,7 @@ module.exports = {
       name: 'admin',
       email: 'admin@mail.com',
       phone: '081233221',
-      password: 'admin',
+      password: '$2a$10$vNjkVunmuPepAbwqdqXsous5H5X.oWjm8eWIK5r0ALc97k6PK1kR2',
       role: 'admin',
       createdAt: new Date(),
       updatedAt: new Date()
